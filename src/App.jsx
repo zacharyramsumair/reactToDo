c99eb760d14a1fd4b250b8117a2a0fd0f1949b5a
Zachary <ramsumairzachary@gmail.com>
2023-03-13 22:47:39 -0400

    todo list works with local storage, everything besides mantaining checked or not

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,65 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 
 function App() {
+
 	let [todos, setTodos] = useState([]);
 	let [current, setCurrent] = useState("");
+
+
+  useEffect(() => {
+    console.log("getting")
+    if(localStorage.getItem("todos")){
+      setTodos(JSON.parse(localStorage.getItem("todos")))
+
+    }
+  }, []);
+
+
+
 
 	function handleChange(e) {
 		e.preventDefault();
 		setCurrent(e.target.value);
+	}
+	function handleCheck(key) {
+		// e.preventDefault();
+    // setTodos((prev) => [...prev, {todo: current, key:nanoid(), checked:!{}}]);
+    
 	}
 
 	function updateTodos(e) {
 		e.preventDefault();
 
 		
-		setTodos((prev) => [...prev, {todo: current, key:nanoid()}]);
+		setTodos((prev) => [...prev, {todo: current, key:nanoid(), checked:false}]);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    // setLocalStorage()
+
+
 		setCurrent("");
 	}
 
 
   function deleteTodo(key){
-    console.log()
+    console.log(todos)
 
     setTodos(prev =>{
       return prev.filter((todo)=> todo.key !== key )
     })
+
+    // setLocalStorage(JSON.stringify(todos.filter((todo)=> todo.key !== key)))
+    localStorage.setItem('todos', JSON.stringify(todos.filter((todo)=> todo.key !== key)));
   }
 
 	let todoElements =
 		todos.length > 0
 			? todos.map((todo) => (
 					<li key={todo.key}>
-						<input type="checkbox" id={todo.key} name={todo.key} value={todo.todo} />
-						<label for={todo.key}>{todo.todo}</label>
+						<input type="checkbox" id={todo.key} name={todo.key} checked={todo.checked} onChange={()=> handleCheck(key)} />
+						<label htmlFor={todo.key}>{todo.todo}</label>
+
+            {/* {todo.todo} */}
 
 						<span>
 							<button onClick={()=> deleteTodo(todo.key)}>Delete</button>
@@ -40,6 +67,16 @@ function App() {
 					</li>
 			  ))
 			: null;
+
+
+  // function setLocalStorage(){
+  //   console.log(todos)
+  //   localStorage.setItem('todos', JSON.stringify(todos));
+
+  // }
+
+
+
 
 	return (
 		<main className="app">
